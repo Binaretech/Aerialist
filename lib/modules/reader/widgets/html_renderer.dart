cd1bd092004223f3cdf4db3b3c7faade8503f9f8
Angel Afonso <angelafonso60@gmail.com>
2021-03-09 22:01:26 -0400

    test

--- lib/modules/reader/widgets/html_renderer.dart
+++ lib/modules/reader/widgets/html_renderer.dart
@@ -1,35 +1,46 @@
-import 'package:html/dom.dart' show Element;
+import 'package:html/parser.dart' as parser;
+import 'package:html/dom.dart' as dom;
 import 'package:Aerialist/modules/reader/bloc/reader_bloc.dart';
-import 'package:flutter/material.dart' hide Element;
-import 'package:flutter_html/flutter_html.dart';
-import 'package:flutter_html/html_parser.dart';
+import 'package:flutter/material.dart';
+import 'package:epubx/epubx.dart';
 
 class HtmlRenderer extends StatelessWidget {
   HtmlRenderer({required this.data});
 
   final String data;
+	
 
-  Widget Function(RenderContext, Widget, Map<String, String>, Element?)
-      imgRenderer(ReaderLoadedState state) {
-    return (context, child, attributes, element) {
-      final image = state.bookRef.Content!.Images![attributes['src']]!
-          .getContentFileEntry()
-          .content;
-      return Image.memory(image);
-    };
-  }
+
+	Widget renderHtmlElement(dom.Element element) {
+		switch(element.localName) {
+			case 'img':
+
+				return 
+		}
+	}
+
+  List<Widget> renderHtmlList(Map<String, EpubByteContentFile> images ,dom.Element element) {
+		switch(element.localName) {
+				case 'section':
+				case 'div':
+						return element
+								.nodes
+								.map((element) => element.nodes.length > 1? renderHtmlList(images, element):renderHtmlElement(element))
+								.toList<Widget>();
+			}
+	}
 
   @override
   Widget build(BuildContext context) {
     final state = ReaderBloc.of(context).state as ReaderLoadedState;
+    final document =
+        parser.parse("<body><div>asdads<p>123123</p></div></body>");
 
-    return SingleChildScrollView(
-      child: Html(
-        data: data,
-        customRender: {
-          'img': imgRenderer(state),
-        },
-      ),
+    return PageView(
+      scrollDirection: Axis.horizontal,
+      children: document.body!.children
+          .map((element) => renderHtml(element))
+          .toList(),
     );
   }
 }
